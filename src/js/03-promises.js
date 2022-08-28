946f6a55bd3d1774b6293103a7ffc2cc208aae21
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2022-08-28 18:09:10 +0300

    start

---
+++ src/js/03-promises.js
@@ -0,0 +1,16 @@
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
