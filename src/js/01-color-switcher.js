89e010ecf661d574a340c5f8a558a8060bb34b58
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2022-08-31 17:42:56 +0300

    add task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,21 @@
+const btnStart = document.querySelector ('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+const bodyEl = document.querySelector(`body`)
 
-
+let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => bodyEl.style.backgroundColor = getRandomHexColor(), 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
